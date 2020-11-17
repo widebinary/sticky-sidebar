c5eb84bb6219de236a1b79a12e02e88b5fc53ee5
Sukanta Das <69782529+widebinary@users.noreply.github.com>
2020-11-17 22:43:40 +0530

    Create file.php

---
+++ file.php
@@ -0,0 +1,6 @@
+<div class="sidebar" id="myHeader">
+<a class="current"  href="Page1.html">Page1</a>
+<a class="current"  href="Page2.html">Page2</a>
+<a class="current"  href="Page3.html">Page3</a>
+</div>
+<script async="async" type="text/javascript" src="navigation.js"></script>
